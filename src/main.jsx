bdc9431e94c356655efee85438e759d88271f69a
samuel2315s <samuvanegasq23@gmail.com>
2026-08-05 15:33:49 -0500

    se realizo unos cambios en app.jsx y en contactocard.jsx y se agregarion unos estilos

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// React: la librería principal
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// ReactDOM: dibuja React en el navegador
+import App from './App.jsx'; 
+// Importamos el componente raíz (nuestra Agenda ADSO)
+
+import './index.css'; 
+// Importamos los estilos globales
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+    {/* Aquí montamos nuestra Agenda ADSO */}
+  </React.StrictMode>,
+);
